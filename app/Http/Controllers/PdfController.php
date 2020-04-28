79152e69100e2e8b4092d0fadf6d0513449c90f3
hacktiv <hacktiv@Hacktiv8s-MacBook-Air.local>
2020-04-28 16:12:32 +0700

    2 page tcpdf with image on bottom right and center

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Elibyy\TCPDF\Facades\TCPDF;
+
+class PdfController extends Controller
+{
+    public function index()
+    {
+        $html = '<div style="text-align:right;"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br />
+        <img src="free.png" alt="test alt attribute" width="100" height="100" border="0" />
+        </div>';
+        $pdf = new TCPDF();
+        $pdf::SetTitle('Image');
+        $pdf::AddPage();
+        $pdf::writeHTML($html, true, false, true, false, '');
+        $pdf::AddPage();
+        $html = '<div style="text-align:center;"><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br />
+        <img src="free.png" alt="test alt attribute" width="100" height="100" border="0" />
+        </div>';
+        $pdf::writeHTML($html, true, false, true, false, '');
+        $pdf::Output('image.pdf');
+    }
+
+    
+}
